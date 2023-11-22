faf5e84041387ce764797b767acabdaf83fc819b
yizhang84 <yi.zhang@bbc.co.uk>
2023-11-22 15:52:09 +0000

    update model

--- model/src/main/java/com.yi.maxirail.model/MainTest.java
+++ model/src/main/java/com.yi.maxirail.model/MainTest.java
@@ -3,5 +3,6 @@ package com.yi.maxirail.model;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("this is model project");
+        System.out.println("this is model project 2");
     }
 }
